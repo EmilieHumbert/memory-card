0ebf18c5af155caed5b07ad354eaf5abf9a65c7a
emilie <emi.m.humbert@googlemail.com>
2020-10-27 20:11:12 +0100

    Create Cards component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
 
+import Cards from "./components/Cards";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Cards />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Cards.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cards = () => {
+  return <div>Hello world!</div>;
+};
+
+export default Cards;
